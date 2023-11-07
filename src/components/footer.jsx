e54dda9b3ce40b3cc3eb9d00fd38e2ccc0f448a9
Sneha1708 <727722euit178@skcet.ac.in>
2023-11-07 14:31:07 +0530

    commit

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const footer = () => {
-  return <div>footer</div>;
+  return (
+    <div>
+      <h1>CopyRights@2023</h1>
+    </div>
+  );
 };
 
 export default footer;
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const main = () => {
-  return <div>main</div>;
+  return (
+    <div>
+      <form>
+        <h1>Email</h1>
+        <h1>Password</h1>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default main;
